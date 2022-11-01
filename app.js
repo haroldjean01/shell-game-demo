6aae1c582a6fdce516cff62ee8292aabe7514c1c
Harold Jean <haroldjean38@yahoo.com>
2022-11-01 15:22:36 -0400

    started with events

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /* Imports */
-const shellButtonOne = document.getElementById('shell-button-one');
-const shellButtonTwo = document.getElementById('shell-button-two');
-const shellButtonThree = document.getElementById('shell-button-three');
 
 /* Get DOM Elements */
+const leftShellButton = document.getElementById('right-shell-button');
+const middleShellButton = document.getElementById('middle-shell-button');
+const rightShellButton = document.getElementById('right-shell-button');
+const pearl = document.getElementById('pearl');
+
+const leftShellImg = document.getElementById('left-shell-img');
+const middleShellImg = document.getElementById('middle-shell-img');
+const rightShellImg = document.getElementById('right-shell-img');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
 
 /* State */
+let wins = 0;
+let total = 0;
 
 /* Events */
-
+leftShellButton.addEventListener('click', () => {
+    total++;
+    resetpearls;
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
